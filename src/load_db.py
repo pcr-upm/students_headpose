fd0011630261550793fdc00a7e68b105a8af8fbc
Iñigo Sanz <isanztorres@outlook.es>
2024-05-30 19:40:05 +0200

    load_db.py funciona, base de train

---
+++ src/load_db.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import os
+import sys
+sys.path.append(os.getcwd())
+from tqdm import tqdm
+from images_framework.src.datasets import Database
+
+def load_annotations(anns_file):
+    """
+    Load ground truth annotations according to each database.
+    """
+    print('Open annotations file: ' + str(anns_file))
+    if os.path.isfile(anns_file):
+        pos = anns_file.rfind('/') + 1
+        path = "" #anns_file[:pos]
+        file = anns_file[pos:]
+        db = file[:file.find('_ann')]
+        datasets = [subclass().get_names() for subclass in Database.__subclasses__()]
+        with open(anns_file, 'r', encoding='utf-8') as ifs:
+            lines = ifs.readlines()
+            anns = []
+            for i in tqdm(range(len(lines)), file=sys.stdout):
+                parts = lines[i].strip().split(',')
+                if parts[0] == '@':
+                    db = parts[1]
+                if parts[0] == '#' or parts[0] == '@':
+                    continue
+                idx = next((idx for idx, subset in enumerate(datasets) if db in subset), None)
+                if idx is None:
+                    raise ValueError('Database does not exist')
+                seq = Database.__subclasses__()[idx]().load_filename(path, db, lines[i])
+                if len(seq.images) == 0:
+                    continue
+                anns.append(seq)
+        ifs.close()
+    else:
+        raise ValueError('Annotations file does not exist')
+    return anns
